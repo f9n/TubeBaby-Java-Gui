162e7baf6ba96d334b8cc88601d87e8935ac38ca
pleycpl <h4rvey@riseup.net>
2016-12-28 19:02:32 +0300

    Updated Main.java file

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
 	
+import application.util.ConfirmBox;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -12,6 +13,10 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			primaryStage.setTitle("Tüp Bebek Sistemi");
+			primaryStage.setOnCloseRequest(e -> {
+	            e.consume();
+	            this.closedProgram(primaryStage);
+	        });
 			Parent root = FXMLLoader.load(getClass().getResource("/application/view/IndexView.fxml"));
 			Scene scene = new Scene(root, 800, 500);
 			primaryStage.setScene(scene);
@@ -20,7 +25,12 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-	
+	private void closedProgram(Stage window) {
+        Boolean answer = ConfirmBox.display("Exit", "Are you want to exit?");
+        if (answer == true) {
+            window.close();
+        }
+    }
 	public static void main(String[] args) {
 		launch(args);
 	}
