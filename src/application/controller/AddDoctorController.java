7c6aa2afd210129295a6a9dd00702d6d407ae22a
pleycpl <h4rvey@riseup.net>
2016-12-28 19:01:39 +0300

    Updated controller directory

--- src/application/controller/AddDoctorController.java
+++ src/application/controller/AddDoctorController.java
@@ -1,5 +1,49 @@
 package application.controller;
 
-public class AddDoctorController {
+import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
+import application.model.DoctorModel;
+import application.model.IndexModel;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class AddDoctorController implements Initializable {
+
+	public DoctorModel model = new DoctorModel();
+	
+	@FXML
+	private TextField dr_no, dr_name, dr_lname, dr_email, dr_cell_telephone, 
+		hospital_no, dr_hire_date, dr_salary;
+	@FXML
+	private Label status;
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void AddDataDoctorTable() {
+		System.out.print("Ekle butonuna tikladin.");
+		try {
+			if(model.insertDoctor(dr_no.getText(), dr_name.getText(), 
+					dr_lname.getText(), dr_email.getText(), dr_cell_telephone.getText(),
+					hospital_no.getText(), dr_hire_date.getText(), dr_salary.getText())) {
+				status.setText("Okey");
+			} else {
+				status.setText("Not Okey");
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.print("Class");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.print("Sql");
+			e.printStackTrace();
+		}
+	}
 }
--- src/application/controller/IndexController.java
+++ src/application/controller/IndexController.java
@@ -1,104 +1,152 @@
 package application.controller;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import application.model.DoctorModel;
+import application.model.IndexModel;
+import application.util.AlertBox;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-public class IndexController {
+public class IndexController implements Initializable{
+	public IndexModel indexModel = null;
+	@FXML
+	private TextField username, pass, dr_no;
+	
+	@FXML
+	private Label dbStatus;
+	
+	@FXML
+    private TableView<DoctorModel> doctorTable;
+    @FXML
+    private TableColumn<DoctorModel, String>  drIdColumn;
+    @FXML
+    private TableColumn<DoctorModel, String>  drNameColumn;
+    @FXML
+    private TableColumn<DoctorModel, String> drLastNameColumn;
+    @FXML
+    private TableColumn<DoctorModel, String> drEmailColumn;
+    @FXML
+    private TableColumn<DoctorModel, String> drPhoneNumberColumn;
+    @FXML
+    private TableColumn<DoctorModel, String>  hospitalNoColumn;
+    @FXML
+    private TableColumn<DoctorModel, String> drHireDateColumn;
+    @FXML
+    private TableColumn<DoctorModel, Integer> salaryColumn;
+	
+    private ObservableList<DoctorModel> table = FXCollections.observableArrayList();
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		AlertBox.display("Title of Window", "Please write username and password for Database");
+		/*
+		doctorTable.add(new DoctorModel("a", "b", "c", "d", "e", "f", "k", 1));
+		employeeTable.setItems(doctorTable);
+		*/
+	}
+	
+	public void tablolariFiltreleme() {
+		System.out.println("Filtrele panpa ...");
+		System.out.println(dr_no.getText());
+	}
+	public void function1clicked() {
+		
+	}
+	public void function2clicked() {
+		
+	}
+	public void function3clicked() {
+		
+	}
 	public void loginButtonClicked() {
 		System.out.println("User logged in ...");
+		if(username.getText() != null && pass.getText() != null) {
+			indexModel = new IndexModel(username.getText(), pass.getText());
+		} else {
+			AlertBox.display("Title of Window", "Please write username and password");
+		}
+		if(indexModel.isDbConnected()) {
+			dbStatus.setText("Connected");
+		} else {
+			dbStatus.setText("Not Connected");
+		}
 	}
-	public void AddHospital() throws IOException {
-		System.out.println("Adding data to Hospital Table");
+	public void settingButtonClicked() {
+		System.out.println("User Setting logged in ...");
+		indexModel = new IndexModel();
+		if(indexModel.isDbConnected()) {
+			dbStatus.setText("Connected");
+		} else {
+			dbStatus.setText("Not Connected");
+		}
+	}
+	public void CreateScene(String title, String file) throws IOException {
 		Stage primaryStage = new Stage();
-		primaryStage.setTitle("Tüp Bebek Sistemindeki Hastane Tablosuna Veri Ekleme");
+		primaryStage.initModality(Modality.APPLICATION_MODAL);
+		primaryStage.setTitle(title);
 		FXMLLoader loader = new  FXMLLoader();
-		Pane root = loader.load(getClass().getResource("/application/view/AddHospital.fxml").openStream());
+		Pane root = loader.load(getClass().getResource(file).openStream());
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
-		primaryStage.show();
+		primaryStage.showAndWait();
+	}
+	public void AddHospital() throws IOException{
+		System.out.println("Adding data to Hospital Table");
+		CreateScene("Tüp Bebek Sistemindeki Hastane Tablosuna Veri Ekleme",
+				"/application/view/AddHospital.fxml");
 	}
 	public void AddDoctor() throws IOException {
 		System.out.println("Adding data to Doctor Table");
-		Stage primaryStage = new Stage();
-		primaryStage.setTitle("Tüp Bebek Sistemindeki Doktor Tablosuna Veri Ekleme");
-		FXMLLoader loader = new  FXMLLoader();
-		Pane root = loader.load(getClass().getResource("/application/view/AddDoctor.fxml").openStream());
-		Scene scene = new Scene(root);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		CreateScene("Tüp Bebek Sistemindeki Doktor Tablosuna Veri Ekleme",
+				"/application/view/AddDoctor.fxml");
 	}
 	public void AddPatient() throws IOException {
 		System.out.println("Adding data to Patient Table");
-		Stage primaryStage = new Stage();
-		primaryStage.setTitle("Tüp Bebek Sistemindeki Hasta Tablosuna Veri Ekleme");
-		FXMLLoader loader = new  FXMLLoader();
-		Pane root = loader.load(getClass().getResource("/application/view/AddPatient.fxml").openStream());
-		Scene scene = new Scene(root);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		CreateScene("Tüp Bebek Sistemindeki Hasta Tablosuna Veri Ekleme",
+				"/application/view/AddPatient.fxml");
 	}
 	public void AddPatientPartner() throws IOException {
 		System.out.println("Adding data to PatientPartner Table");
-		Stage primaryStage = new Stage();
-		primaryStage.setTitle("Tüp Bebek Sistemindeki Hastane Eþi Tablosuna Veri Ekleme");
-		FXMLLoader loader = new  FXMLLoader();
-		Pane root = loader.load(getClass().getResource("/application/view/AddPatientPartner.fxml").openStream());
-		Scene scene = new Scene(root);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		CreateScene("Tüp Bebek Sistemindeki Hastane Eþi Tablosuna Veri Ekleme",
+				"/application/view/AddPatientPartner.fxml");
 	}
 	public void AddFamily() throws IOException {
 		System.out.println("Adding data to Family Table");
-		Stage primaryStage = new Stage();
-		primaryStage.setTitle("Tüp Bebek Sistemindeki Aile Tablosuna Veri Ekleme");
-		FXMLLoader loader = new  FXMLLoader();
-		Pane root = loader.load(getClass().getResource("/application/view/AddFamily.fxml").openStream());
-		Scene scene = new Scene(root);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		CreateScene("Tüp Bebek Sistemindeki Aile Tablosuna Veri Ekleme",
+				"/application/view/AddFamily.fxml");
 	}
 	public void AddRooms() throws IOException {
 		System.out.println("Adding data to Rooms Table");
-		Stage primaryStage = new Stage();
-		primaryStage.setTitle("Tüp Bebek Sistemindeki Oda Tablosuna Veri Ekleme");
-		FXMLLoader loader = new  FXMLLoader();
-		Pane root = loader.load(getClass().getResource("/application/view/AddRooms.fxml").openStream());
-		Scene scene = new Scene(root);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		CreateScene("Tüp Bebek Sistemindeki Oda Tablosuna Veri Ekleme",
+				"/application/view/AddRooms.fxml");
 	}
 	public void AddPhoneNumber() throws IOException {
 		System.out.println("Adding data to PhoneNumber Table");
-		Stage primaryStage = new Stage();
-		primaryStage.setTitle("Tüp Bebek Sistemindeki Telefon Numarasi Tablosuna Veri Ekleme");
-		FXMLLoader loader = new  FXMLLoader();
-		Pane root = loader.load(getClass().getResource("/application/view/AddPhoneNumber.fxml").openStream());
-		Scene scene = new Scene(root);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		CreateScene("Tüp Bebek Sistemindeki Telefon Numarasi Tablosuna Veri Ekleme",
+				"/application/view/AddPhoneNumber.fxml");
 	}
 	public void AddPill() throws IOException {
 		System.out.println("Adding data to Pill Table");
-		Stage primaryStage = new Stage();
-		primaryStage.setTitle("Tüp Bebek Sistemindeki Ilaç Tablosuna Veri Ekleme");
-		FXMLLoader loader = new  FXMLLoader();
-		Pane root = loader.load(getClass().getResource("/application/view/AddPill.fxml").openStream());
-		Scene scene = new Scene(root);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		CreateScene("Tüp Bebek Sistemindeki Ilaç Tablosuna Veri Ekleme",
+				"/application/view/AddPill.fxml");
 	}
 	public void AddPillPatient() throws IOException {
 		System.out.println("Adding data to PillPatient Table");
-		Stage primaryStage = new Stage();
-		primaryStage.setTitle("Tüp Bebek Sistemindeki Ilaç-Hasta Tablosuna Veri Ekleme");
-		FXMLLoader loader = new  FXMLLoader();
-		Pane root = loader.load(getClass().getResource("/application/view/AddPillPatient.fxml").openStream());
-		Scene scene = new Scene(root);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		CreateScene("Tüp Bebek Sistemindeki Ilaç-Hasta Tablosuna Veri Ekleme",
+				"/application/view/AddPillPatient.fxml");
 	}
 }
